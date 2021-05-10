495daddd07f67ed97bc19eae10859276e9c90620
ParitoshikPaul <pparitoshik@gmail.com>
2021-05-10 21:48:18 +0530

    Added redux support

---
+++ src/reducers/index.ts
@@ -0,0 +1,11 @@
+import { createSelector } from 'reselect';
+import { ActionReducer } from '@ngrx/store';
+import * as fromRouter from '@ngrx/router-store';
+import { environment } from '../../environments/environment';
+import { compose } from '@ngrx/core/compose';
+import { storeFreeze } from 'ngrx-store-freeze';
+import { combineReducers } from '@ngrx/store';
+
+export interface State {
+
+}
