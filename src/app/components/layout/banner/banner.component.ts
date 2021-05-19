a2c397e79f32129042f2948d7dbbaca97ab591c8
ParitoshikPaul <pparitoshik@gmail.com>
2021-05-19 17:53:22 +0530

    Banner Animations

--- src/app/components/layout/banner/banner.component.ts
+++ src/app/components/layout/banner/banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-banner',
@@ -7,11 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
   div: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(this.swapText, 3000);
+    setInterval(this.swapImg, 3000);
+
+    // window.setInterval(, 2000);
   }
   infoText(div) {
     this.div = div;
+  }
+  swapText() {
+    const x = document.getElementById('swap-text');
+    if (x.innerHTML === 'Business') {
+      x.innerHTML = 'Work';
+    } else if (x.innerHTML === 'Work') {
+      x.innerHTML = 'Innovative team';
+    } else {
+      x.innerHTML = 'Business';
+    }
+  }
+  swapImg() {
+    const x = document.getElementById('swap-img');
+    if (x.getAttribute('src') === 'assets/images/landing_rectangle.png') {
+      x.setAttribute('src', 'https://via.placeholder.com/576x605.png');
+    } else if (x.getAttribute('src') === 'https://via.placeholder.com/576x605.png') {
+      x.setAttribute('src', 'assets/images/landing_rectangle.png');
+    }
   }
 }
