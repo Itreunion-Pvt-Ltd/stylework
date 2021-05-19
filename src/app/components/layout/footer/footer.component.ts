0a44e6b9d22b452e5f3182b415652bf6b76c4a97
ParitoshikPaul <pparitoshik@gmail.com>
2021-05-19 19:21:03 +0530

    Footer animation

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  counter = 0;
+  constructor() {
   }
-
+  ngOnInit(): void {
+    setInterval(this.progressBar, 3000);
+  }
+  progressBar() {
+    const x = document.getElementById('progress-circle-border');
+    let count = 0;
+    console.log(this.counter);
+    if (count === 0) {
+      x.classList.add('percentage-25');
+    } else if (x.classList.contains('percentage-25')) {
+      x.classList.add('percentage-50');
+    } else if (x.classList.contains('percentage-50')) {
+      x.classList.add('percentage-75');
+    } else if (x.classList.contains('percentage-75')) {
+      x.classList.add('percentage-100');
+    }
+    count++;
+    this.counter = count;
+  }
 }
